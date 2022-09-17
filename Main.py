a329327f027c87da6385652f6d8b4157776c9f4c
Anandjoe <110010902+Anandjoe@users.noreply.github.com>
2022-09-17 21:04:26 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,49 @@
-from typing import List
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
-  for i in range(1, len(array)):
-    n = array[i]
-    j = i - 1
-    while j>=0 and n < array[j]:
-      array[j+1] = array[j]
-      j -= 1
-    array[j+1] = n
-   
-  return array
+class Queue:
+  def __init__(self):
+    self.head = None
+    self.last = None
+
+  def enqueue(self, data) -> None:
+      last = self.last
+      node = Node(data)
+      if(self.last is None and self.head is None):
+        self.last , self.head = node , node
+      else:
+        self.last.next = node
+        self.last = node
+
+  def dequeue(self) -> None:
+    if self.head is not None:
+      self.head = self.head.next
+    else:
+      print("Queue is Empty")
+
+    if self.head.next is None:
+      self.head = None
+      self.last = None
+
+  def status(self) -> None:
+    ptr = self.head
+    while ptr:
+      print(ptr.data,end = "=>")
+      ptr = ptr.next
+    print("None")
 
 
-# data = [9, 5, 1, 4, 3]
+queue = Queue()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
 input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(insertionSort(data))
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
