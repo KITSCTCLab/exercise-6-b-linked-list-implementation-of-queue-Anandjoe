d13fcc83036e3579f29a7783122c3f9e21009d02
Anandjoe <110010902+Anandjoe@users.noreply.github.com>
2022-09-17 21:12:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,76 +1,49 @@
-class MyCircularQueue:
-    def __init__(self, size: int):
-        self.queue = [0] * size
-        self.size = size
-        self.rear = -1
-        self.front = -1
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
-    def enqueue(self, value: int) -> bool:
-        if self.is_full():
-            return False
-        if self.front == -1:
-            self.front = 0
-            self.rear = 0
-        else:
-            self.rear = (self.rear + 1) % self.size
-        self.queue[self.rear] = value
-        return True
 
-    def dequeue(self) -> bool:
-        if self.is_empty():
-            return False
-        if self.front == self.rear:
-            self.front = -1
-            self.rear = -1
-        else:
-            self.front = (self.front + 1) % self.size
+class Queue:
+  def __init__(self):
+    self.head = None
+    self.last = None
 
-        return True
+  def enqueue(self, data) -> None:
+      last = self.last
+      node = Node(data)
+      if(self.last is None and self.head is None):
+        self.last , self.head = node , node
+      else:
+        self.last.next = node
+        self.last = node
 
-    def get_front(self) -> int:
-        if not self.is_empty():
-            return self.queue[self.front]
-        return -1
+  def dequeue(self) -> None:
+    if self.head == self.last:
+        self.head = self.last = None
+    else:
+        self.head = self.head.next
 
-    def get_rear(self):
-        if not self.is_empty():
-            return self.queue[self.rear]
-        return -1
+    
 
-    def is_empty(self):
-        return self.front == -1
-
-    def is_full(self):
-        return (self.front == -0 and self.rear == (self.size - 1)) or (self.front == (self.rear + 1) % self.size)
+  def status(self) -> None:
+    ptr = self.head
+    while ptr != None:
+      print(ptr.data,end = "=>")
+      ptr = ptr.next
+    print("None")
 
 
 # Do not change the following code
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
-data = []
-for item in input().split(','):
-    item = item.strip()
-    if item == '-':
-        data.append([])
-    else:
-        data.append([int(item)])
-obj = MyCircularQueue(data[0][0])
-result = []
+input_data = input()
+data = input_data.split(',')
 for i in range(len(operations)):
-    if i == 0:
-        result.append(None)
-    elif operations[i] == "enqueue":
-        result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
-        result.append(obj.get_rear())
-    elif operations[i] == "get_front":
-        result.append(obj.get_front())
-    elif operations[i] == "dequeue":
-        result.append(obj.dequeue())
-    elif operations[i] == "is_full":
-        result.append(obj.is_full())
-    elif operations[i] == "is_empty":
-        result.append(obj.is_empty())
-
-print(result)
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
